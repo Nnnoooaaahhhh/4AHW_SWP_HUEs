4798004c09ca872a96d74fa4623241346bd52ac7
Nnnoooaaahhhh <55807513+Nnnoooaaahhhh@users.noreply.github.com>
2020-10-20 22:28:54 +0200

    Add files via upload

--- HolidaysX/src/testMain.java
+++ HolidaysX/src/testMain.java
@@ -1,44 +1,46 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Scanner;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import javax.xml.parsers.*;
 import org.apache.commons.io.IOUtils;
 import org.json.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.stage.Stage;
+import java.sql.*;
 
-public class testMain extends Application{
+public class testMain extends Application {
 
-	static String URL;
+	static String URLdates;
+	static String free = "https://ferien-api.de/api/v1/holidays/BY";
 	static ArrayList<LocalDate> datesL = new ArrayList<LocalDate>();
 	static ArrayList<LocalDate> finalDates = new ArrayList<LocalDate>();
+	static ArrayList<LocalDate> frees = new ArrayList<LocalDate>();
 	static NodeList dates;
 	static int years;
 	static LocalDate start;
 	static LocalDate end;
 	static Scanner scan = new Scanner(System.in);
-	static int yearURL;
+	static int yearURLdates;
+	static int yearURLfree;
 	static int mon = 0;
 	static int tue = 0;
 	static int wed = 0;
 	static int thu = 0;
 	static int fri = 0;
+	static int lifetimeMondays = 0;
+	static int lifetimeTuesdays = 0;
+	static int lifetimeWednesdays = 0;
+	static int lifetimeThursdays = 0;
+	static int lifetimeFridays = 0;
+	
+	static boolean freeDay = false;
 
 	public static void main(String args[]) throws MalformedURLException, JSONException, IOException {
 		testMain a = new testMain();
@@ -46,9 +48,12 @@ public class testMain extends Application{
 		a.daysToNodeList();
 		a.elementsToArrayList();
 		a.datesInAL();
+		a.getFrees();
 		a.testIfEqual();
+		a.database("databaseDates.db");
 		a.ausgabe();
 		launch(args);
+
 	}
 
 	static String getWert(JSONObject json, String key) throws JSONException {
@@ -78,7 +83,7 @@ public class testMain extends Application{
 			start = LocalDate.of(scan.nextInt(), scan.nextInt(), scan.nextInt());
 			end = start.plusYears(years);
 			scan.close();
-			yearURL = start.getYear();
+			yearURLdates = start.getYear();
 		} catch (Exception e) {
 		}
 	}
@@ -101,8 +106,8 @@ public class testMain extends Application{
 	}
 
 	void datesInAL() throws MalformedURLException, JSONException, IOException {
-		for (int y = 0; y <= years; y++) {
-			JSONObject json = new JSONObject(IOUtils.toString(new URL(rightURL()), Charset.forName("UTF-8")));
+		for (int y = 0; y < years; y++) {
+			JSONObject json = new JSONObject(IOUtils.toString(new URL(rightURL1()), Charset.forName("UTF-8")));
 			String day = null;
 			String date;
 			for (int x = 0; x < 4; x++) {
@@ -118,26 +123,30 @@ public class testMain extends Application{
 				if (x == 3) {
 					day = "Pfingstmontag";
 				}
-				date = getWert(json, day);		
+				date = getWert(json, day);
 				finalDates.add(LocalDate.parse(date));
+
 			}
 		}
 	}
 
-	static String rightURL() {
-		URL = "https://feiertage-api.de/api/?jahr=" + Integer.toString(yearURL) + "&nur_land=BY";
-		yearURL++;
-		return URL;
+	static String rightURL1() {
+		URLdates = "https://feiertage-api.de/api/?jahr=" + Integer.toString(yearURLdates) + "&nur_land=BY";
+		yearURLdates++;
+		return URLdates;
 	}
 
 	void testIfEqual() {
 		DayOfWeek weekDay;
 		try {
 			for (int i = 0; i < i + 1; i++) {
+				if (frees.contains(start)) {
+					freeDay = true;
+				}
 				if (start.equals(end)) {
 					break;
 				}
-				if (finalDates.contains(start)) {
+				if (finalDates.contains(start) && !freeDay) {
 					weekDay = start.getDayOfWeek();
 					if (weekDay.equals(DayOfWeek.MONDAY)) {
 						mon++;
@@ -156,47 +165,146 @@ public class testMain extends Application{
 					}
 				}
 				start = start.plusDays(1);
+				freeDay = false;
 			}
 		} catch (Exception e) {
 			System.out.println("Something went wrong");
 		}
 	}
-	
+
 	void ausgabe() {
-		System.out.println("Holidays on a monday: " + mon);
+		System.out.println("\n" + "Holidays on a monday: " + mon);
 		System.out.println("Holidays on a tuesday: " + tue);
 		System.out.println("Holidays on a wednesday: " + wed);
 		System.out.println("Holidays on a thursday: " + thu);
 		System.out.println("Holidays on a friday: " + fri);
+		 
+        System.out.print("\n" + "Lifetime Mondays :" + lifetimeMondays);
+        System.out.print("\n" + "Lifetime Tuesdays :" + lifetimeTuesdays);
+        System.out.print("\n" + "Lifetime Wednesdays :" + lifetimeWednesdays);
+        System.out.print("\n" + "Lifetime Thursdays :" + lifetimeThursdays);
+        System.out.print("\n" + "Lifetime Fridays :" + lifetimeFridays);
 	}
-	
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void start(Stage stage) throws Exception {
 		NumberAxis yAxis = new NumberAxis();
-		CategoryAxis xAxis= new CategoryAxis();
+		CategoryAxis xAxis = new CategoryAxis();
 		String mondays = "Mondays";
 		String tuesdays = "Tuesdays";
 		String wednesdays = "Wedndesday";
 		String thursdays = "Thursdays";
 		String fridays = "Fridays";
-		
 		stage.setTitle("Holidays per weekday during " + (end.minusYears(years)) + " - " + end);
-		BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
-	    xAxis.setLabel("Weekday");       
-	    yAxis.setLabel("Holidays");
-	    XYChart.Series series = new XYChart.Series();
-	    series.getData().add(new XYChart.Data(mondays, mon));
-	    series.getData().add(new XYChart.Data(tuesdays, tue));
-	    series.getData().add(new XYChart.Data(wednesdays, wed));
-	    series.getData().add(new XYChart.Data(thursdays, thu));
-	    series.getData().add(new XYChart.Data(fridays, fri));
-	    series.setName("Holidays per weekday");
-	    
-	    Scene scene = new Scene (bc, 800, 600);
-	    bc.getData().addAll(series);
-	    stage.setScene(scene);
-	    stage.show();
+		BarChart<String, Number> bc = new BarChart<String, Number>(xAxis, yAxis);
+		xAxis.setLabel("Weekday");
+		yAxis.setLabel("Holidays");
+		XYChart.Series series = new XYChart.Series();
+		series.getData().add(new XYChart.Data(mondays, mon));
+		series.getData().add(new XYChart.Data(tuesdays, tue));
+		series.getData().add(new XYChart.Data(wednesdays, wed));
+		series.getData().add(new XYChart.Data(thursdays, thu));
+		series.getData().add(new XYChart.Data(fridays, fri));
+		series.setName("Holidays per weekday");
+		Scene scene = new Scene(bc, 800, 600);
+		bc.getData().addAll(series);
+		stage.setScene(scene);
+		stage.show();
 	}
+
+	void getFrees() throws JSONException, IOException {
+
+		String start;
+		String end;
+		LocalDate temp;
+		JSONArray ob = new JSONArray(
+				IOUtils.toString(new URL("https://ferien-api.de/api/v1/holidays/BY"), Charset.forName("UTF-8")));
+		for (int i = 0; i < ob.length(); i++) {
+			JSONObject job = ob.getJSONObject(i);
+			start = "";
+			end = "";
+			start = (String) job.get("start");
+			start = start.split("T")[0];
+			end = (String) job.get("end");
+			end = end.split("T")[0];
+			temp = LocalDate.parse(start);
+			for (int y = 0; y < y + 1; y++) {
+				frees.add(temp);
+				if (temp.equals(LocalDate.parse(end))) {
+					break;
+				}
+				temp = temp.plusDays(1);
+			}
+		}
+	}
+
+	void database(String databasename) {
+		String url = "jdbc:sqlite:" + databasename;
+		String sql = "CREATE TABLE if not exists testdata ( date text, mondays int, tuesdays int, wednesdays int, thursdays int, fridays int, years int)";
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			Statement stmt = conn.createStatement();
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		sql = "INSERT INTO testdata(date, mondays, tuesdays, wednesdays, thursdays, fridays, years) VALUES(?,?,?,?,?,?,?)";
+
+		try {
+			Connection conn = this.connect(databasename);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, LocalDate.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd")));
+			pstmt.setInt(2, mon);
+			pstmt.setInt(3, tue);
+			pstmt.setInt(4, wed);
+			pstmt.setInt(5, thu);
+			pstmt.setInt(6, fri);
+			pstmt.setInt(7, years);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+        sql = "SELECT * FROM testdata";  
+        
+        try {  
+            Connection conn = this.connect(databasename);  
+            Statement stmt  = conn.createStatement();  
+            ResultSet rs    = stmt.executeQuery(sql);  
+            int temp;
+            while (rs.next()) {  
+                System.out.print(rs.getString("date") +  "\t");   
+                temp = rs.getInt("mondays");
+                System.out.print(temp + "\t");
+                lifetimeMondays = lifetimeMondays + temp;
+                temp = rs.getInt("tuesdays");
+                System.out.print(temp + "\t");
+                lifetimeTuesdays = lifetimeTuesdays + temp;
+                temp = rs.getInt("wednesdays");
+                System.out.print(temp + "\t");
+                lifetimeWednesdays = lifetimeWednesdays + temp;
+                temp = rs.getInt("thursdays");
+                System.out.print(temp + "\t");
+                lifetimeThursdays = lifetimeThursdays + temp;
+                temp = rs.getInt("fridays");
+                System.out.print(temp + "\t");
+                System.out.println(rs.getInt("years"));
+                lifetimeFridays = lifetimeFridays + temp;
+            }
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        }  
+	}
+
+	private Connection connect(String databasename) {
+		String url = "jdbc:sqlite:" + databasename;
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+
 }
