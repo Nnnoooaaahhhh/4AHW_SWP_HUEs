6f6a9a009912197a21ef58ff17550025a1df7300
Nnnoooaaahhhh <55807513+Nnnoooaaahhhh@users.noreply.github.com>
2021-04-05 19:01:42 +0200

    Add files via upload

--- StockMarketProject/src/methods.java
+++ StockMarketProject/src/methods.java
@@ -1,27 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-import org.apache.commons.io.IOUtils;
 import org.json.*;
 
 
 public class methods {
 
 	static String symbol;
-	static String DBurl = "jdbc:sqlite:infos.db";;
+	static String DBurl = "jdbc:sqlite:infos.db";
 	static ArrayList<String> dates = new ArrayList<String>();
 	static ArrayList<Float> values = new ArrayList<Float>();
 	static JSONObject b;
+	static ArrayList<String> ticks = new ArrayList<String>();
+	static String link;
+	static String startDate;
+	static String endDate;
+	static String linkSize;
 	String firstDate;
 	static String key;
+	static Connection conn;
 
-	void connect(String abbr) throws JSONException, MalformedURLException, IOException {
-		String link = IOUtils.toString(new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="
-				+ abbr + "&outputsize=full&apikey="+key), Charset.forName("UTF-8"));
+	void connect(String link) throws JSONException, MalformedURLException, IOException {
 		JSONObject ob = new JSONObject(link);
 		b = ob.getJSONObject("Time Series (Daily)");
 		for (int i = 0; i < b.names().length(); i++) {
@@ -30,37 +33,29 @@ public class methods {
 			}
 		}
 		dates.sort(null);
-		
-		
 	}
+	
 
-	String decide() {
+	void decide() throws IOException {
 		Scanner a = new Scanner(System.in);
-		System.out.println("Abbreviation (e.g.: AAPL, TSLA, IBM, ...): ");
-		symbol = a.next();
-		System.out.println("API-KEY: ");
-		key = a.next();
+		FileReader Freader = new FileReader("ticks.txt");
+		BufferedReader BReader = new BufferedReader(Freader);
+		while(BReader.ready()) {
+			ticks.add(BReader.readLine());
+		}
+		key = ticks.get(0);
+		startDate = ticks.get(1);
+		endDate = ticks.get(2);
+		linkSize = ticks.get(3);
+		BReader.close();
 		a.close();
-		return symbol;
 	}
+	 
 
 	void database() throws SQLException {
 		String sql;
 		Connection conn = DriverManager.getConnection(DBurl);
-		sql = "CREATE TABLE if not exists " + symbol + " (date text PRIMARY KEY, amount text)";
-		try {
-			Statement stmt = conn.createStatement();
-			stmt.execute(sql);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		sql = "CREATE TABLE if not exists " + symbol+"avg" + " (date text PRIMARY KEY, avg text)";
-		try {
-			Statement stmt = conn.createStatement();
-			stmt.execute(sql);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
+
 		sql = "CREATE TABLE if not exists " + symbol+"avgAdj" + " (date text PRIMARY KEY, avg text)";
 		try {
 			Statement stmt = conn.createStatement();
@@ -76,18 +71,6 @@ public class methods {
 			System.out.println(e.getMessage());
 		}
 		
-		sql = "INSERT OR REPLACE INTO " + symbol + " (date, amount) values (?,?)";
-		for (int i = 0; i < dates.size(); i++) {
-			try {
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, dates.get(i));
-				pstmt.setString(2, b.getJSONObject(dates.get(i)).get("4. close").toString());
-				pstmt.executeUpdate();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-			
-	}
 		sql = "INSERT OR REPLACE INTO " + symbol +"Adj" + " (date, amount) values (?,?)";
 		for (int i = 0; i < dates.size(); i++) {
 			try {
@@ -97,43 +80,22 @@ public class methods {
 				pstmt.executeUpdate();
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
-			}
-			
+			}	
 	}
 		
 		dates.clear();
-		sql = "select date from " +symbol;
+		sql = "select date from " +symbol+"Adj";
 		try {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
-			
 			while (rs.next()) {
-				dates.add(rs.getString("date"));
-				
+				dates.add(rs.getString("date"));		
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 		String sql2;
 		String test;
-		 sql = "INSERT OR REPLACE INTO " + symbol+"avg" + " (date, avg) values (?,?)";
-		  for (int i = 0; i < dates.size(); i++) {
-			  sql2 = "select avg(amount) as temp from (select amount from "+symbol+" where date <= '"+ dates.get(i)+ "' order by date desc limit 200)";
-				Statement stmt = conn.createStatement();
-				ResultSet rs2 = stmt.executeQuery(sql2);
-				rs2.next();
-				try {
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					pstmt.setString(1, dates.get(i));
-					test = rs2.getString("temp");
-					pstmt.setString(2, test);
-					pstmt.executeUpdate();
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-					System.out.println(e.getMessage());
-				}
-			}
 			 sql = "INSERT OR REPLACE INTO " + symbol+"avgAdj" + " (date, avg) values (?,?)";
 			  for (int i = 0; i < dates.size(); i++) {
 				  sql2 = "select avg(amount) as temp from (select amount from "+symbol+ "Adj"+" where date <= '"+ dates.get(i)+ "' order by date desc limit 200)";
@@ -152,5 +114,6 @@ public class methods {
 						System.out.println(e.getMessage());
 					}
 				}
+			  conn.close();
 		}
 }
--- StockMarketProject/src/testiMain.java
+++ StockMarketProject/src/testiMain.java
@@ -1,109 +1,140 @@
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javafx.scene.image.WritableImage;
-import org.json.JSONException;
+import org.apache.commons.io.IOUtils;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
 import javafx.stage.Stage;
 
 public class testiMain extends Application{
-
-	public static void main(String[] args) throws JSONException, MalformedURLException, IOException, SQLException {
-		methods a = new methods();
-		a.connect(a.decide());
-		a.database();
-		launch(args);
-	}
-
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void start(Stage stage) throws Exception {
-		ArrayList<Float> amounts = new ArrayList<Float>();
-		stage.setTitle("Stocks of " + methods.symbol);
-		CategoryAxis xAxis = new CategoryAxis();
-		NumberAxis yAxis = new NumberAxis();
-		LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-		
-		XYChart.Series series1 = new XYChart.Series();
-		XYChart.Series series2 = new XYChart.Series();
-		series1.setName("Close-Values");
-		series2.setName("AVG-Values");
-		Connection conn = DriverManager.getConnection(methods.DBurl);
-		String sql = "SELECT * from " + methods.symbol +"Adj"+ " order by date asc";
-		String sql2 = "SELECT * from " + methods.symbol+"avgAdj" + " order by date asc";
+	static String fileSave;
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
 		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			while (rs.next()) {
-				series1.getData().add(new XYChart.Data(rs.getString("date"), Float.parseFloat(rs.getString("amount"))));
+			methods b = new methods();
+			b.decide();
+			for(int i = 4; i < b.ticks.size(); i++) {
+				b.dates.clear();
+				b.symbol = b.ticks.get(i);
+				b.link = IOUtils.toString(new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="
+						+ b.ticks.get(i) + "&outputsize=" + b.linkSize +"&apikey="+b.key), Charset.forName("UTF-8"));
+				b.connect(b.link);
+				b.database();	
 			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			launch(args);
 		}
-		try {
-			Statement stmt2 = conn.createStatement();
-			ResultSet rs2 = stmt2.executeQuery(sql2);
-		while (rs2.next()) {
-			series2.getData().add(new XYChart.Data(rs2.getString("date"), Float.parseFloat(rs2.getString("avg"))));
+		catch(Exception e) {
+			System.out.println("Ein Fehler ist aufgetreten!");
 		}
-		
-	} catch (SQLException e) {
-		System.out.println(e.getMessage());
+		finally {
+			System.exit(0);
+		}
 	}
-		lineChart.setCreateSymbols(false);
+
+	@SuppressWarnings({ "rawtypes", "unchecked", "static-access" })
+	public void start(Stage stage) throws Exception {
+		methods a = new methods();
+		Connection conn = DriverManager.getConnection(methods.DBurl);
 		
-		Scene scene = new Scene(lineChart,800,600);
-		lineChart.getData().addAll(series1, series2);
-		sql = "SELECT * from " + methods.symbol +"Adj" + " order by date desc limit 1";
-		sql2 = "SELECT * from " + methods.symbol+"avgAdj" + " order by date desc limit 1";
-		float lastClose = 0;
-		float lastAVG = 0;
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			Statement stmt2 = conn.createStatement();
-			ResultSet rs2 = stmt2.executeQuery(sql2);
-			while (rs.next()) {
-				lastClose = Float.parseFloat(rs.getString("amount"));
-				lastAVG = Float.parseFloat(rs2.getString("avg"));
+		for(int i = 4; i < a.ticks.size(); i++) {
+			XYChart.Series series1 = new XYChart.Series();
+			XYChart.Series series2 = new XYChart.Series();
+			ArrayList<Float> amounts = new ArrayList<Float>();
+			CategoryAxis xAxis = new CategoryAxis();
+			NumberAxis yAxis = new NumberAxis();
+			LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);	
+			stage.setTitle("Stocks of " + a.ticks.get(i));
+			series1.setName("Close-Values");
+			series2.setName("AVG-Values");
+			String sql = "SELECT * from " + a.ticks.get(i)+"Adj"+ " where date > '"+a.startDate +"' and date < '"+ a.endDate+"' order by date asc";
+			String sql2 = "SELECT * from " + a.ticks.get(i)+"avgAdj" + " where date > '" + a.startDate + "' and date < '" + a.endDate +"' order by date asc";
+			try {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				
+				while (rs.next()) {
+					series1.getData().add(new XYChart.Data(rs.getString("date"), Float.parseFloat(rs.getString("amount"))));
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
 			}
+			try {
+				Statement stmt2 = conn.createStatement();
+				ResultSet rs2 = stmt2.executeQuery(sql2);
+			while (rs2.next()) {
+				series2.getData().add(new XYChart.Data(rs2.getString("date"), Float.parseFloat(rs2.getString("avg"))));
+			}
+			
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		if(lastClose > lastAVG) {
-			scene.getStylesheets().add("green.css");
-		}else {
-			scene.getStylesheets().add("red.css");
-		}
-		series2.getNode().setStyle("-fx-stroke: black; ");
-		
-		sql = "SELECT * from " + methods.symbol + "Adj"+" order by amount desc";
-		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) {
-				amounts.add(Float.parseFloat(rs.getString("amount")));
+			lineChart.setCreateSymbols(false);
+			Scene scene = new Scene(lineChart,800,600);
+			lineChart.getData().addAll(series1, series2);
+			sql = "SELECT * from " + a.ticks.get(i)+"Adj" + " where date > '" + a.startDate + "' and date < '" + a.endDate +"' order by date desc limit 1";
+			sql2 = "SELECT * from " + a.ticks.get(i)+"avgAdj" + " where date > '" + a.startDate + "' and date < '" + a.endDate +"' order by date desc limit 1";
+			float lastClose = 0;
+			float lastAVG = 0;
+			try {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				Statement stmt2 = conn.createStatement();
+				ResultSet rs2 = stmt2.executeQuery(sql2);
+				while (rs.next()) {
+					lastClose = Float.parseFloat(rs.getString("amount"));
+					lastAVG = Float.parseFloat(rs2.getString("avg"));
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
 			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			if(lastClose > lastAVG) {
+				scene.getStylesheets().add("green.css");
+			}else {
+				scene.getStylesheets().add("red.css");
+			}
+			series2.getNode().setStyle("-fx-stroke: black; ");
+			
+			sql = "SELECT * from " + a.ticks.get(i) + "Adj"+" where date > '" + a.startDate + "' and date < '" + a.endDate +"' order by amount desc";
+			try {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				while (rs.next()) {
+					amounts.add(Float.parseFloat(rs.getString("amount")));
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			amounts.sort(null);
+			yAxis.setAutoRanging(false);
+			yAxis.setLowerBound(amounts.get(0)*0.9);
+			yAxis.setUpperBound(amounts.get(amounts.size()-1)*1.1);
+			stage.setScene(scene);
+			Parent root = scene.getRoot();
+			scene.setRoot(root);
+			fileSave = LocalDate.now().toString()+a.ticks.get(i);
+			File theDir = new File("stocks/"+a.ticks.get(i)+"/");
+			if (!theDir.exists()){
+			    theDir.mkdirs();
+			}
+			saveAsPng(scene, "stocks/"+a.ticks.get(i)+"/" +fileSave+".png");
+			series1.getData().removeAll();
+			series2.getData().removeAll();
+			amounts.clear();
 		}
-		amounts.sort(null);
-		yAxis.setAutoRanging(false);
-		yAxis.setLowerBound(amounts.get(0)*0.9);
-		yAxis.setUpperBound(amounts.get(amounts.size()-1)*1.1);
-		stage.setScene(scene);
-		stage.show();
-		
-		saveAsPng(scene, methods.symbol+ LocalDate.now().toString()+".png");
-		
+		conn.close();
+		stage.close();
+		System.exit(0);	
 		}
 	
 		void saveAsPng(Scene scene, String path){
@@ -115,7 +146,6 @@ public class testiMain extends Application{
                 e.printStackTrace();
             }
 		}
-
 }
 
 
